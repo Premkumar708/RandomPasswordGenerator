68d1fe7ff1fbd15d24b3748da6e87f0deb4142b2
p-k-v <prem.v.kumar2002@gmail.com>
2023-11-22 08:10:10 +0530

    updated commit

--- index.js
+++ index.js
@@ -7,26 +7,36 @@ const number = document.querySelector('#numbers')
 
 const copy_api = document.querySelector("#copy");
 const submit = document.querySelector("#submit")
+copy_api.addEventListener('click',copy)
 
 submit.addEventListener('click',generatePassword)
-
 const char_letters = "abcdefghijklmnopqrstuvwxyz";
 const char_numbers = "1234567890";
 const char_symbols ="!@#$%^&*()";
 
-let password = "";
 let length = length_el.value
-let char = "";
 
 function generatePassword() {
-    char += upperCase.checked ? char_letters.toUpperCase(): "";
-    char += lowerCase.checked ? char_letters.toLowerCase(): "";
-    char += symbol.checked ? char_symbols: "";
-    char += number.checked ? char_numbers: "";
+    let char = "";
+    let password = "";
+   if(upperCase.checked) char += char_letters.toUpperCase();
+   if(lowerCase.checked) char += char_letters;
+   if(number.checked) char += char_numbers;
+   if(symbol.checked) char += char_symbols
     
     for(let i = 0 ; i < length; i++){
-        let rand = Math.floor(Math.random() * length)
+        let rand = Math.floor(Math.random() * char.length)
         password += char[rand]
     }
     password_el.value = password;
+    if(password_el.value="undefined"){
+        password_el.value = ''
+    }
 }   
+
+async function copy () {
+    if(navigator.clipboard){
+        await navigator.clipboard.writeText(password_el.value);
+        alert('copied')
+    }
+}
